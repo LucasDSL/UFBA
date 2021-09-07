09f79329c2d3ce015d44ea6a6ed77380d817a4a0
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2021-09-06 21:42:24 -0300

    flip_bit() done

--- Arquitetura de Computadores/bit_ops.c
+++ Arquitetura de Computadores/bit_ops.c
@@ -7,7 +7,7 @@ unsigned get_bit(unsigned x,
                  unsigned n) {
     
     unsigned new_x = x>>n;
-    return (new_x & 0x1);
+    return (new_x & 0b1);
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
@@ -20,7 +20,9 @@ void set_bit(unsigned * x,
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
-    // YOUR CODE HERE
+    unsigned mask = 0x1<<n;
+    *x = *x ^ mask;
+    
 }
 
 void test_get_bit(unsigned x,
@@ -34,6 +36,18 @@ void test_get_bit(unsigned x,
     }
 }
 
+void test_flip_bit(unsigned x,
+        unsigned n,
+        unsigned expected) {
+    unsigned o = x;
+    flip_bit(&x, n);
+    if(x!=expected) {
+        printf("flip_bit(0x%08x,%u): 0x%08x, expected 0x%08x\n",o,n,x,expected);
+    } else {
+        printf("flip_bit(0x%08x,%u): 0x%08x, correct\n",o,n,x);
+    }
+}
+
 int main(){
     printf("\nTesting get_bit()\n\n");
     test_get_bit(0b1001110,0,0);
@@ -42,4 +56,11 @@ int main(){
     test_get_bit(0b11011,3,1);
     test_get_bit(0b11011,2,0);
     test_get_bit(0b11011,9,0);
+
+    printf("\nTesting flip_bit()\n\n");
+    test_flip_bit(0b1001110,0,0b1001111);
+    test_flip_bit(0b1001110,1,0b1001100);
+    test_flip_bit(0b1001110,2,0b1001010);
+    test_flip_bit(0b1001110,5,0b1101110);
+    test_flip_bit(0b1001110,9,0b1001001110);
 }
