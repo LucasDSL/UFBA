7120a28fb3b74919b0adbbe895655bd63c096dcd
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2021-09-08 21:42:17 -0300

    adding question inside exercise 10 and removing tests from main

--- Laboratório de Programação I/04 Chamadas-Escopos-Rotinas/e10.c
+++ Laboratório de Programação I/04 Chamadas-Escopos-Rotinas/e10.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+//Exercício 10 (The Huxley, 2021). Um belo hotel na praia tem apartamentos especialmente decorados e cobra os valores indicados na tabela a seguir. Com o fim do verão, o movimento caiu e o hotel resolveu oferecer 15% de desconto para hospedagens de 3 ou mais dias. Escreva uma função chamada CalculaHospedagem que receba como parâmetro o tipo de apartamento desejado e a quantidade de diárias, calcule e retorne o valor total a ser pago. Individual - 125 / Suíte Dupla - 140 / Suíte tripla - 180.
 float calculaHospedagem(char tipoApartamento[15], int totalDiarias){
     float total;
     if(tipoApartamento == "Individual"){
@@ -18,8 +18,4 @@ float calculaHospedagem(char tipoApartamento[15], int totalDiarias){
     }
 
     return total;
-}
-
-int main(){
-    printf("%f\n", calculaHospedagem("Suíte Tripla", 5));
 }
