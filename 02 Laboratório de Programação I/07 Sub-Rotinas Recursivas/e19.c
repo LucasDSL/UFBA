17a49ba1db780196f6008cff2f985cb371ee1dc4
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2021-09-17 16:46:40 -0300

    19 AEEEEE

--- 02 Laboratório de Programação I/07 Sub-Rotinas Recursivas/e19.c
+++ 02 Laboratório de Programação I/07 Sub-Rotinas Recursivas/e19.c
@@ -2,21 +2,20 @@
 #include<stdlib.h>
 #include<math.h>
 //[adaptado de Paes (2016)]. Escreva uma função recursiva que receba uma string e o tamanho dessa string como parâmetro e retorne a mesma string com os caracteres na ordem contrária.
-void inverteString(char *s, int len){
-    char aux = *(s+len-1);
-    if(*s == *(s+((int)(len/2)))){
-        *(s+len-1) = *s;
-        *s = aux;
+void inverte(char *s, int pos, int len){
+    if(pos >= len - 1){
+        return *(s+pos);
     }
     else{
-        *(s+len-1) = *s;
-        *s = aux;
-        inverteString(s+1, len);
+        char aux = *(s+pos);
+        *(s+pos) = *(s+len-1);
+        *(s+len-1) = aux;
+        inverte(s, pos+1, len-1);
     }
 }
 
 int main(){
-    char lucass[] = {'l', 'u', 'c', 'a', 's', 's'};
-    inverteString(&lucass, 6);
-    printf("A string lucas invertida eh %s\n", lucass);
+    char lucas[] = "lucas";
+    inverte(lucas, 0, 5);
+    printf("A string lucas invertida eh %s\n", lucas);
 }
