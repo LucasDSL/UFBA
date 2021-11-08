d3da30fecdbb65fc48b87bdf24ddae3e430daf70
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2021-11-08 17:27:29 -0300

    Mais funções - find min, max e height

--- 02 Estrutura de Dados e Algoritmos I/07 Árvores/01 binary_tree.c
+++ 02 Estrutura de Dados e Algoritmos I/07 Árvores/01 binary_tree.c
@@ -16,6 +16,12 @@ tree *create_tree();
 void insert_node(tree*, node*, node*);
 node *insert_value(node *, int);
 void print_tree_pre_order(node *);
+void print_tree_post_order(node *);
+void print_tree_in_order(node *);
+int search(node *, int);
+int find_min(node *);
+int find_max(node *);
+int find_height(node *);
 
 int main(){
     tree *T = create_tree();
@@ -25,7 +31,15 @@ int main(){
     T->root = insert_value(T->root, 3);
     T->root = insert_value(T->root, 12);
     T->root = insert_value(T->root, 25);
+    printf("Print pre order:\n");
     print_tree_pre_order(T->root);
+    printf("\n");
+    printf("Print in order:\n");
+    print_tree_in_order(T->root);
+    printf("\n");
+    printf("Print post order:\n");
+    print_tree_post_order(T->root);
+    printf("\n Height of previous tree: %d\n", find_height(T->root));
     return EXIT_SUCCESS;
 }
 
@@ -34,8 +48,59 @@ void print_tree_pre_order(node *root){ // Root - left - right
         printf(" %d", root->value);
         print_tree_pre_order(root->left_child);
         print_tree_pre_order(root->right_child);
-
     }
+}
+
+void print_tree_post_order(node *root){
+    if(root != NULL){
+        print_tree_post_order(root->left_child);
+        print_tree_post_order(root->right_child);
+        printf(" %d", root->value);
+    }
+}
+
+void print_tree_in_order(node *root){
+    if(root != NULL){
+        print_tree_in_order(root->left_child);
+        printf(" %d", root->value);
+        print_tree_in_order(root->right_child);
+    }
+}
+int search(node* root, int data){
+    if(root == NULL){
+        return 0;
+    }
+    else if(root->value == data){
+        return 1;
+    }
+    else if(root->value <= data){
+        return search(root->right_child, data);
+    }
+    else return search(root->left_child, data);
+}
+
+int find_min(node *root){
+    if(root == NULL){
+        printf("Empty tree\n");
+        return -1;
+    }
+    else if(root->left_child == NULL){
+        return root->value;
+    }
+    return find_min(root->left_child);
+    /* node* current = root;
+    Iterative version
+    while(current->left != NULL){
+        current = current -> left;
+    }
+    return current->value; */
+}
+
+int find_height(node *root){
+    if(root == NULL) return -1;
+    int left_height = find_height(root->left_child);
+    int right_height = find_height(root->right_child);
+    return left_height < right_height ? right_height + 1 : left_height + 1;
 }
 node *create_node(int num){
     node *n = (node *) malloc(sizeof(node));
@@ -61,6 +126,7 @@ node *insert_value(node *root, int value){
     }
     return root;
 }
+
 /* void insert_node(tree *t, node *sub_tree, node* n){ // Implementação do professor
     if(t->root == NULL){
         t->root = n;
