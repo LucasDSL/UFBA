8ceb9aed98bdb4c399fec233527e8b06eb6ab6bd
LucasDSL <lucasdslima2002@gmail.com>
2022-09-10 15:51:40 -0300

    feat: python class properties

--- 2022.2/programacao-orientada-a-objetos/3_p2_classes/6_ret_prop.py
+++ 2022.2/programacao-orientada-a-objetos/3_p2_classes/6_ret_prop.py
@@ -2,10 +2,26 @@
 Defina a classe Retangulo com dois atributos, base e altura (inicializados durante a criação do objeto), e uma propriedade, area, que é computada a partir dos dois atributos mas não pode ser alterada.'''
 class Retangulo:
   def __init__(self, base, altura):
-    self.base = base
-    self.altura = altura
-    self.razao = base / altura 
-    
+    self._base = base
+    self._altura = altura
+
+  def get_base(self):
+    return self._base 
+  def set_base(self, new_base):
+    self._base = new_base
+	
+  def get_altura(self):
+    return self._altura
+  def set_altura(self, value):
+    self._altura = value
+
+  def get_area(self): 
+    return self._base * self._altura
+
+  base = property(get_base, set_base)
+  altura = property(get_altura, set_altura)
+  area = property(get_area, None)
+
 ### Testes
 r1 = Retangulo(3, 4)
 assert r1.area == 12
